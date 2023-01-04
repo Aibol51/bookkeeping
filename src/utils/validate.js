786b5fdb465f504aa2a86fb3ae12e3bff3f86c7a
Haolaaa <halamatg@gmail.com>
2023-01-04 14:58:29 +0400

    表单验证代码提取到了单独的文件中，这样可以在不同的地方复用

---
+++ src/utils/validate.js
@@ -0,0 +1,20 @@
+export const emailValidator = (value, callback) => {
+    const regex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if (!regex.test(value)) {
+        return "请填写正确的邮箱地址";
+    }
+};
+
+export const passwordValidator = (value, callback) => {
+    if (value.length < 6) {
+        return "密码长度不能小于6位";
+    }
+};
+
+export const confirmPasswordValidator = (value, callback) => {
+    console.log(value);
+    if (value !== formData.password) {
+        return "两次输入密码不一致";
+    }
+};
